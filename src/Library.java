55f07c707512942572cfd30591742687976cbd73
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-20 10:53:08 +0100

    feat added basic methods to Library class

--- src/Library.java
+++ src/Library.java
@@ -5,7 +5,17 @@ public class Library {
 
     List<Book> books = new ArrayList<Book>();
 
+    public void addBook(Book book){
+        books.add(book);
+        System.out.println(book + "Added to the Library");
+    }
 
+    public void deleteBook(Book book){
+        books.remove(book);
+        System.out.println(book + " deleted successfully");
+    }
 
-
+    public List<Book> getBooks() {
+        return books;
+    }
 }
