83eadaaa9eea4c743c1605e8accbf26197552dad
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-20 13:08:02 +0100

    fix deleted method deletebook

--- src/Library.java
+++ src/Library.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Library {
 
-    List<Book> books = new ArrayList<Book>();
+    private List<Book> books = new ArrayList<Book>();
 
     public void addBook(Book book){
         if(books.contains(book)){
@@ -11,27 +12,27 @@ public class Library {
         }else{
             books.add(book);
             System.out.println(book + " Added to the Library");
-
         }
+        books.sort(Comparator.comparing(Book::getTitle));
     }
 
-    public void deleteBook(Book book){
-        books.remove(book);
-        System.out.println(book + " deleted successfully");
-    }
 
     public boolean deleteBookByTitle(String title){
         for(int i = 0 ; i <  books.size(); i++){
-            if(books.get(i).getTitle().equals(title)){
+            if(books.get(i).getTitle().equalsIgnoreCase(title)){
                 books.remove(i);
+                books.sort(Comparator.comparing(Book::getTitle));
                 return true;
             }
+
         }
+
         return false;
     }
 
     public void removeByIndex(int index){
         books.remove(index);
+        books.sort(Comparator.comparing(Book::getTitle));
     }
 
     public Book getBookAt(int index){
@@ -39,13 +40,13 @@ public class Library {
     }
 
     public void addBookAtIndex(int index, Book book){
-
         if(books.contains(book)){
             System.out.println("Book already exists");
+            return;
         }else{
             books.add(index, book);
         }
-
+        books.sort(Comparator.comparing(Book::getTitle));
     }
 
     public List<Book> getBooks() {
