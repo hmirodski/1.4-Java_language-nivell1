1429a03668adcc89c1080ae9ba8b22ca644334d8
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-24 20:46:27 +0100

    feat  test sortedList

--- tests/LibraryTests.java
+++ tests/LibraryTests.java
@@ -31,8 +31,8 @@ public class LibraryTests {
         library.addBook(book1);
         library.addBook(book2);
 
-        assertEquals("Dubliners", library.getBooks().get(0).getTitle());
-        assertEquals("Ulysses", library.getBooks().get(1).getTitle());
+        assertEquals("Dubliners", library.getBookAt(0).getTitle());
+        assertEquals("Ulysses", library.getBookAt(1).getTitle());
     }
 
     @Test
@@ -75,6 +75,47 @@ public class LibraryTests {
         Book book2 = new Book("Ulysses", "James Joyce");
         library.addBook(book2);
         assertEquals(2, library.getBooks().size());
+
+    }
+
+    @Test
+    public void decreasedListSize(){
+        Library library = new Library();
+        Book book1 = new Book("Dubliners", "James Joyce");
+        Book book2 = new Book("Ulysses", "James Joyce");
+
+        library.addBook(book1);
+        library.addBook(book2);
+
+        assertEquals(2, library.getBooks().size());
+
+        library.deleteBookByTitle("Dubliners");
+        assertEquals(1, library.getBooks().size());
+
+        library.deleteBookByTitle("Ulysses");
+        assertEquals(0, library.getBooks().size());
+
+    }
+
+    @Test
+    public void sortedList(){
+        Library library = new Library();
+        Book book1 = new Book("Dubliners", "James Joyce");
+        Book book2 = new Book("Ulysses", "James Joyce");
+        Book book3 = new Book("Harry Potter", "J K Rowling");
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+
+        assertEquals("Dubliners", library.getBookAt(0).getTitle());
+
+        assertEquals("Harry Potter", library.getBookAt(1).getTitle());
+        assertEquals("Ulysses", library.getBookAt(2).getTitle());
+
+        library.deleteBookByTitle("Dubliners");
+        assertEquals("Harry Potter", library.getBookAt(0).getTitle());
+        assertEquals("Ulysses", library.getBookAt(1).getTitle());
 
     }
 
