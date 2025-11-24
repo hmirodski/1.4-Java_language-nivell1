82f00fb3f5dd0c9688ad596e2451e652e8a7e2ac
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-24 21:22:29 +0100

    feat calculoDni

--- src/exercici2/calculoDni.java
+++ src/exercici2/calculoDni.java
@@ -2,4 +2,16 @@ package exercici2;
 
 public class calculoDni {
 
+    private static final char[] letters = {
+            'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F',
+            'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S',
+            'Q', 'V', 'H', 'L', 'C', 'K', 'E'
+    };
+
+    public static char calcularLetra(int dninumber){
+        int index = dninumber % 23;
+        return letters[index];
+    }
+
+
 }
