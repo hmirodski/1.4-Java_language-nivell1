2fad396744a950eee4c5c8049f7c1cb1274eb29b
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-24 18:07:49 +0100

    feat: test added

---
+++ tests/LibraryTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LibraryTests {
+
+    @Test
+    public void bookListIsNotNull() {
+        Library library = new Library();
+        assertNotNull(library.getBooks());
+    }
+
+    @Test
+    public void listSize() {
+        Library library = new Library();
+        Book book1 = new Book("Dubliners", "James Joyce");
+        Book book2 = new Book("Ulysses", "James Joyce");
+
+        library.addBook(book1);
+        library.addBook(book2);
+
+        assertEquals(2, library.getBooks().size());
+    }
+}
